6b3c251221a5ec18669126c38f77a899ed14ebee
sbcesar <sbcesar@iesrafaelalberti.es>
2024-01-31 13:39:48 +0100

    tasks to do - create function compare(t:Time): Int - create function copy():Time - create function copy(t:Time):Time - create function summarize(t:Time):Time? - create function subtract(t:Time):Time? - create function greaterThan(t:Time):Boolean - create function lowerThan(t:Time): Boolean 13:40 31/01/2024

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,49 @@
 fun main() {
+    try {
 
-    println("Introduce la hora: ")
-    val hora = readln().toIntOrNull() ?: 0
+        val tiempoFijo = Tiempo(20,15,20)
 
-    println("Introduce los minutos: ")
-    val minuto = readln().toIntOrNull() ?: 0
+        println("Introduce la hora a incrementar: ")
+        val hora = readln().toInt()
 
-    println("Introduce los segundos: ")
-    val segundo = readln().toIntOrNull() ?: 0
+        println("Introduce los minutos a incrementar: ")
+        val minuto = readln().toInt()
 
-    val tiempo1 = Tiempo(hora,minuto,segundo)
+        println("Introduce los segundos a incrementar: ")
+        val segundo = readln().toInt()
 
-    println("La hora es: $tiempo1")
+        val tiempoAIncrementar = Tiempo(hora,minuto,segundo)
 
+        println("El tiempo es $tiempoFijo")
+
+        if (tiempoFijo.incrementar(tiempoAIncrementar)) {
+            println("Tiempo incrementado: $tiempoFijo")
+        } else {
+            throw IndexOutOfBoundsException("*** ERROR *** - El tiempo sobrepasa el limite maximo")
+        }
+
+        println("Introduce la hora a decrementar: ")
+        val hora1 = readln().toInt()
+
+        println("Introduce los minutos a decrementar: ")
+        val minuto1 = readln().toInt()
+
+        println("Introduce los segundos a decrementar: ")
+        val segundo1 = readln().toInt()
+
+        val tiempoADecrementar = Tiempo(hora1,minuto1,segundo1)
+
+        println("El tiempo es $tiempoFijo")
+
+        if (tiempoFijo.decrementar(tiempoADecrementar)) {
+            println("El tiempo decrementado es: $tiempoFijo")
+        } else {
+            throw IndexOutOfBoundsException("*** ERROR *** - El tiempo es menor al limite minimo")
+        }
+
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    } catch (e: IndexOutOfBoundsException) {
+        println(e.message)
+    }
 }
--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -2,16 +2,64 @@ class Tiempo(private var horas: Int, private var minutos: Int, private var segun
 
     init {
         require(horas in 0..23) { "La hora debe estar desde 0 a 23" }
-        require(minutos in 0..59) { "Los minutos deben estar desde 0 a 59" }
-        require(segundos in 0..59) { "Los segundos deben estar desde 0 a 59" }
 
-        val minutosAjustados = (minutos + segundos / 60) % 60
-        val horasAjustadas = (horas + minutos / 60) % 24
+        if (minutos > 60 || segundos > 60) {
+            val minutosAjustados = (minutos + segundos / 60) % 60
+            val horasAjustadas = (horas + minutos / 60) % 24
 
-        this.horas = horasAjustadas
-        this.minutos = minutosAjustados
-        this.segundos %= 60
+            this.horas = horasAjustadas
+            this.minutos = minutosAjustados
+            this.segundos %= 60
+        }
     }
+
+    constructor(horas: Int) : this(horas,0,0) {
+        this.horas = horas
+    }
+
+    constructor(horas: Int,minutos: Int) : this(horas,minutos,0) {
+        this.horas = horas
+        this.minutos = minutos
+    }
+
+    fun incrementar(t:Tiempo): Boolean {
+        segundos += t.segundos
+        minutos += t.minutos
+        horas += t.horas
+
+        if (segundos >= 60 || minutos >= 60 || horas >= 24) {
+            minutos += segundos / 60
+            horas += minutos / 60
+
+            segundos %= 60
+            minutos %= 60
+            horas %= 24
+
+            return false
+        }
+
+        return true
+    }
+
+    fun decrementar(t: Tiempo): Boolean {
+        segundos -= t.segundos
+        minutos -= t.minutos
+        horas -= t.horas
+
+        if (segundos < 0 || minutos < 0 || horas < 0) {
+            minutos -= (segundos / 60 + 1)
+            horas -= (minutos / 60 + 1)
+
+            segundos = 60 + (segundos % 60)
+            minutos = 60 + (minutos % 60)
+            horas = 24 + (horas % 24)
+
+            return false
+        }
+
+        return true
+    }
+
 
     override fun toString(): String {
         return String.format("%02d:%02d:%02d", horas, minutos, segundos)
