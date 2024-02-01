6a78b5022e92bcc09d8623468d76d70665523d42
sbcesar <sbcesar@iesrafaelalberti.es>
2024-02-01 12:55:15 +0100

    done - all the implementations
    tasks to do
    - document code
    - ceate menu at main to optimize
    01/02/2024

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,7 @@ fun main() {
 
         val tiempoFijo = Tiempo(20,15,20)
 
+        //INCREMENTO
         println("Introduce la hora a incrementar: ")
         val hora = readln().toInt()
 
@@ -22,6 +23,7 @@ fun main() {
             throw IndexOutOfBoundsException("*** ERROR *** - El tiempo sobrepasa el limite maximo")
         }
 
+        //DECREMENTO
         println("Introduce la hora a decrementar: ")
         val hora1 = readln().toInt()
 
@@ -41,6 +43,91 @@ fun main() {
             throw IndexOutOfBoundsException("*** ERROR *** - El tiempo es menor al limite minimo")
         }
 
+        //COMPARACION
+        println("Introduce la hora a comparar: ")
+        val hora2 = readln().toInt()
+
+        println("Introduce los minutos a comparar: ")
+        val minuto2 = readln().toInt()
+
+        println("Introduce los segundos a comparar: ")
+        val segundo2 = readln().toInt()
+
+        val tiempoComparar = Tiempo(hora2,minuto2,segundo2)
+
+        val comparacion = tiempoFijo.comparar(tiempoComparar)
+
+        when (comparacion) {
+            -1 -> println("$tiempoComparar es menor que $tiempoFijo")
+            0 -> println("$tiempoComparar es igual que $tiempoFijo")
+            1 -> println("$tiempoComparar es menor que $tiempoFijo")
+        }
+
+        //COPIA
+        println("COPIA: ${tiempoFijo.copiar()}")
+
+        //COPIA 2
+        println("COPIA 2: ${tiempoFijo.copiar(tiempoComparar)}")
+
+        //SUMA
+        println("Introduce la hora a sumar: ")
+        val hora3 = readln().toInt()
+
+        println("Introduce los minutos a sumar: ")
+        val minuto3 = readln().toInt()
+
+        println("Introduce los segundos a sumar: ")
+        val segundo3 = readln().toInt()
+
+        val tiempoSumar = Tiempo(hora3,minuto3,segundo3)
+
+        val tiempoTotal = tiempoFijo.sumar(tiempoSumar)
+
+        if (tiempoTotal == null) {
+            throw IndexOutOfBoundsException("La hora sobrepasa el limite maximo (23:59:59)")
+        } else {
+            println("El resultado es $tiempoTotal")
+        }
+
+        //RESTA
+        println("Introduce la hora a restar: ")
+        val hora4 = readln().toInt()
+
+        println("Introduce los minutos a restar: ")
+        val minuto4 = readln().toInt()
+
+        println("Introduce los segundos a restar: ")
+        val segundo4 = readln().toInt()
+
+        val tiempoRestar = Tiempo(hora4,minuto4,segundo4)
+
+        val tiempoTotal2 = tiempoFijo.resta(tiempoRestar)
+
+        if (tiempoTotal2 == null) {
+            throw IndexOutOfBoundsException("La hora es menor el limite minimo (00:00:00)")
+        } else {
+            println("El resultado de restar $tiempoFijo - $tiempoRestar es $tiempoTotal2")
+        }
+
+        //MAYOR QUE O MENOR QUE
+        println("Introduce la hora para comparar si es mayor o menor: ")
+        val hora5 = readln().toInt()
+
+        println("Introduce los minutos para comparar si es mayor o menor: ")
+        val minuto5 = readln().toInt()
+
+        println("Introduce los segundos para comparar si es mayor o menor: ")
+        val segundo5 = readln().toInt()
+
+        val tiempoComparar1 = Tiempo(hora5,minuto5,segundo5)
+
+        if (tiempoFijo.esMayorQue(tiempoComparar1)) {
+            println("$tiempoFijo es mayor que $tiempoComparar1")
+        } else if (tiempoFijo.esMenorQue(tiempoComparar1)) {
+            println("$tiempoFijo es menor que $tiempoComparar1")
+        } else {
+            println("$tiempoFijo es igual que $tiempoComparar1")
+        }
     } catch (e: IllegalArgumentException) {
         println(e.message)
     } catch (e: IndexOutOfBoundsException) {
--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -56,10 +56,78 @@ class Tiempo(private var horas: Int, private var minutos: Int, private var segun
 
             return false
         }
-
         return true
     }
 
+    fun comparar(t:Tiempo): Int {
+        return if (t.horas < horas && t.minutos < minutos && t.segundos < segundos) {
+            -1
+        } else if (t.horas > horas && t.minutos > minutos && t.segundos > segundos) {
+            1
+        } else {
+            0
+        }
+    }
+
+    //Esta funcion es util si deseas hacer calculos con el objeto sin modificar el original
+    fun copiar(): Tiempo {
+        return Tiempo(horas,minutos,segundos)
+    }
+
+    //Esta funcion es util si quiero actualizar los valores del objeto con otro objeto, al haber otra funcion copiar esto se le denomina saturacion
+    fun copiar(t: Tiempo): Tiempo {
+        this.horas = t.horas
+        this.minutos = t.minutos
+        this.segundos = t.segundos
+        return this
+    }
+
+    fun sumar(t:Tiempo): Tiempo? {
+        val sumaSegundos = this.segundos + t.segundos
+        val sumaMinutos = this.minutos + t.minutos + sumaSegundos / 60
+        val sumaHoras = this.horas + t.horas + sumaMinutos / 60
+
+        val segundosResultado = sumaSegundos % 60
+        val minutosResultado = sumaMinutos % 60
+        val horasResultado = sumaHoras % 24
+
+        if (sumaHoras >= 24) {
+            return null
+        }
+
+        return Tiempo(horasResultado, minutosResultado, segundosResultado)
+    }
+
+    fun resta(t:Tiempo): Tiempo? {
+        val restaSegundos = this.segundos - t.segundos
+        var restaMinutos = this.minutos - t.minutos
+        var restaHoras = this.horas - t.horas
+
+        if (restaSegundos < 0) {
+            restaMinutos -= 1
+        }
+        if (restaMinutos < 0) {
+            restaHoras -= 1
+        }
+
+        if (restaHoras < 0) {
+            return null
+        }
+
+        val segundosResultado = if (restaSegundos < 0) 60 + restaSegundos else restaSegundos
+        val minutosResultado = if (restaMinutos < 0) 60 + restaMinutos else restaMinutos
+        val horasResultado = if (restaHoras < 0) 24 + restaHoras else restaHoras
+
+        return Tiempo(horasResultado, minutosResultado, segundosResultado)
+    }
+
+    fun esMayorQue(t: Tiempo): Boolean {
+        return comparar(t) == 1
+    }
+
+    fun esMenorQue(t: Tiempo): Boolean {
+        return comparar(t) == -1
+    }
 
     override fun toString(): String {
         return String.format("%02d:%02d:%02d", horas, minutos, segundos)
